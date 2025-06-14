660a6ae1fbe72c06dfb5895dd039a5ead7f233c0
—͞HɪᴍᴜㅤMᴀʟꜱ !i <166631945+HIMU106x@users.noreply.github.com>
2025-06-14 22:16:48 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+{
+  "bot": {
+    "token": "YOUR_TELEGRAM_BOT_TOKEN",
+    "prefix": ".",
+    "language": "en",
+    "admins": [6800909814, ],
+    "commandNotFoundMessage": "❌ Unknown command. Use /help to view all commands.",
+    "logGroup": -1001234567890
+  }
+}
